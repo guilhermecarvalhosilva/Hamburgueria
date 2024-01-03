a50fa43b188c3109ba2eb6b2f1842847e171ce26
Guilherme Carvalho <guilhermecarvalhodev693@gmail.com>
2024-01-03 20:44:13 -0300

    atulização do codigo

--- script/menu.js
+++ script/menu.js
@@ -1,13 +1,14 @@
 var menu = document.getElementById(`menu`)
-var header = window.document.body
+var header = document.querySelectorAll(`.cabecalho`)[0]
+
 
 
 function showMenu(){
     if(menu.classList.contains(`showMenu`)) {
         menu.classList.remove(`showMenu`)
-        header.classList.remove(`showBody`)
+        header.classList.remove(`showHeader`)
     } else {
         menu.classList.add(`showMenu`)
-        header.classList.add(`showBody`)
+        header.classList.add(`showHeader`)
     }
 }
