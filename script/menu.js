702c17abb5647416fcf4385c2f0a199599c8a75d
Guilherme Carvalho <guilhermecarvalhodev693@gmail.com>
2024-01-05 13:09:20 -0300

    Atualização do código

--- script/menu.js
+++ script/menu.js
@@ -1,14 +1,17 @@
 const menu = document.getElementById(`menu`) // variavel do elemento menu
 const header = document.querySelectorAll(`.cabecalho`)[0] // variavel do header
+const body = window.document.body
 
 
 //função que serve para abrir e fechar o menu da landing page
 function showMenu(){
-    if(menu.classList.contains(`showMenu`)) {
-        menu.classList.remove(`showMenu`)
-        header.classList.remove(`showHeader`)
+    if(menu.classList.contains(`ativo`)) {
+        menu.classList.remove(`ativo`)
+        header.classList.remove(`ativo`)
+        body.classList.remove(`ativo`)
     } else {
-        menu.classList.add(`showMenu`)
-        header.classList.add(`showHeader`)
+        menu.classList.add(`ativo`)
+        header.classList.add(`ativo`)
+        body.classList.add(`ativo`)
     }
 }
